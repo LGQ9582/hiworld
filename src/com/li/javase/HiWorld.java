0c4dd83a08bd2daef1e057bd6f22f1889ea27708
LGQ9582 <rjgcli@163.com>
2020-02-07 12:19:16 +0800

    test

--- src/com/li/javase/HiWorld.java
+++ src/com/li/javase/HiWorld.java
@@ -3,6 +3,6 @@ package com.li.javase;
 public class HiWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Github!");
+        System.out.println("Hello Github4545544545!");
     }
 }
